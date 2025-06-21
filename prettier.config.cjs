c9a34bf59cd139988476bbc03c62a30e2a11fd0c
Kirill M. <some@mail.com>
2025-06-21 20:23:20 +0300

    prettier

---
+++ prettier.config.cjs
@@ -0,0 +1,36 @@
+module.exports = {
+	printWidth: 100,
+	quoteProps: "consistent",
+	semi: true,
+	singleQuote: false,
+	tabWidth: 4,
+	trailingComma: "all",
+	useTabs: true,
+	overrides: [
+		{
+			// Some JSON files are only ever used by JSON5-aware tools
+			files: ["tsconfig*.json", ".vscode/*.json"],
+			options: {
+				parser: "json5",
+				tabWidth: 2,
+				quoteProps: "preserve",
+			},
+		},
+		{
+			files: "*.json",
+			options: {
+				tabWidth: 2,
+				quoteProps: "preserve",
+			},
+		},
+		{
+			// YAML formatting should not use tabs, and use a 2-space indent instead
+			files: ["*.yaml", "*.yml"],
+			options: {
+				tabWidth: 2,
+				useTabs: false,
+				quoteProps: "preserve",
+			},
+		},
+	],
+};
